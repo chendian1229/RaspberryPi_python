34e58d2e88c8913702fa81b0e6fa152e040b8b31
NEO <su5858@vip.qq.com>
2017-03-31 09:13:47 +0800

    PCF8591_AD

---
+++ PCF8591_IIC/AD.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+import smbus
+import time
+
+address=0x48
+A0=0x40
+A1=0x41
+A2=0x42
+A3=0x43
+bus=smbus.SMBus(1)
+
+
+if __name__=='__main__':
+    try:
+        while True:
+            bus.write_byte(address,A3)
+            value=bus.read_byte(address)
+            print("AOUT:%1.3f "%(value*3.3/255))
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print('exit')
+
+
+
--- 爬虫/picture.py
+++ 爬虫/picture.py
@@ -1,6 +1,6 @@
 import urllib.request
 
-response=urllib.request.urlopen('https://placekitten.com/g/500/600')
+response=urllib.request.urlopen('https://placekitten.com/g/1024/1024')
 img=response.read()
 
 with open('cat.jpg','wb') as f:
