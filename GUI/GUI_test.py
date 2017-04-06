3fa6b033b7421027268b2c92c002927488660220
NEO <su5858@vip.qq.com>
2017-04-06 21:54:43 +0800

    显示不正常，未找到原因

--- GUI/GUI_test.py
+++ GUI/GUI_test.py
@@ -4,18 +4,18 @@ class Application(Frame):
 
     def __int__(self,master):
         super(Application,self).__int__(master)
-        self.grid()
+        #self.grid()
         self.creat_widgets()
 
     def creat_widgets(self):
-        self.label1=Lable(self,text="This is a window")
+        self.label1=Label(self,text='This is a window')
+        self.pack()
         self.label1.grid(row=0,column=0,sticky=W)
+        self.button1=Button(self,text='Quit',command=self.quit)
 
 
-for value in range(0,10):
-    value=value+1
 root=Tk()
 root.title('UART-Value')
-#root.geometry('300*100')
+root.geometry('300x100')
 app=Application(root)
 app.mainloop()
