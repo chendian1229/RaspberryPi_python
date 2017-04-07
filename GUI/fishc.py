ad6bf5e5a1fa4ef08f0317d90177ba9cfae132b3
NEO <su5858@vip.qq.com>
2017-04-07 18:26:29 +0800

    字体变化

--- GUI/fishc.py
+++ GUI/fishc.py
@@ -4,14 +4,18 @@ import time
 root=Tk()
 root.title('To fishc')
 
-
-
-label1=Label(root,text="This is a \n cat !")
+label1=Label(root,text='This is a cat !',font=('Tempus Sans ITC',20),fg='Red')
 label1.pack(side=LEFT)
 
 photo=PhotoImage(file="cat.png")
 
 label2=Label(root,image=photo)
 label2.pack(side=RIGHT)
+
+
+
+
+    
+
 
 mainloop()
