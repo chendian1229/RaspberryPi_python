c770d01eceaf7b6fdf52fdfb6203dee718f0f003
NEO <su5858@vip.qq.com>
2017-04-09 20:50:53 +0800

    UDP_ESP8266

---
+++ TCP_UDP/UDP_SEND.py
@@ -0,0 +1,17 @@
+import socket   
+import time
+ 
+DST_IP = "192.168.1.111"
+DST_PORT = 1112
+ 
+#建立Socket，SOCK_DGRAM表示Socket类型为UDP
+print("Starting socket: UDP...")
+socket_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+ 
+#向目的地址和端口，发送数据
+print("Sending package to %s:%d..." %(DST_IP, DST_PORT))
+dst_addr = (DST_IP, DST_PORT)
+while True:
+        socket_udp.sendto("Hello UDP!",dst_addr)
+        time.sleep(1) 
+        continue
