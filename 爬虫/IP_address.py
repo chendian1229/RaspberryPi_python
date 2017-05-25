50ca02321687b57a7f122a45556c0b8259064631
NEO <su5858@vip.qq.com>
2017-05-25 20:58:48 +0800

    some change

--- 爬虫/IP_address.py
+++
@@ -1,9 +0,0 @@
-import requests
-url="http://m.ip138.com/ip.asp?ip="
-try:
-    r=requests.get(url+"121.22.24.7")
-    r.raise_for_status()
-    r.encoding=r.apparent_encoding
-    print(r.text[-500:])
-except:
-    print("Mission failed")
