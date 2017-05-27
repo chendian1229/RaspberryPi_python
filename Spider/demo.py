ea9c2f74a1553bc07a01b077c876ba0e349112c2
NEO <su5858@vip.qq.com>
2017-05-27 22:00:07 +0800

    BeautifulSoup库的样历

--- Spider/demo.py
+++ Spider/demo.py
@@ -5,3 +5,13 @@ r=requests.get("http://python123.io/ws/demo.html")
 demo=r.text
 soup=BeautifulSoup(demo,"html.parser")
 
+for parent in soup.a.parents:
+    if parent is None:
+        print(parent)
+    else:
+        print(parent.name)
+
+
+print(soup.prettify())
+
+
