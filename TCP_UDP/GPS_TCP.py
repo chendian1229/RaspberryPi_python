b92c167b34b8a7685f7a1a3c8ad37d0aea380556
NEO <su5858@vip.qq.com>
2017-05-27 21:56:24 +0800

    GPS_TCP测试，读取实时时间与经纬度

--- TCP_UDP/GPS_TCP.py
+++ TCP_UDP/GPS_TCP.py
@@ -1,7 +1,12 @@
 from socket import *
 from time import ctime
+import re
 
-HOST="192.168.1.109"
+GPS_time=r'(\d{6}\.\d{3}\,)(\d{4}\.\d{5}\,N\,)(\d{5}\.\d{5}\,E)'
+GPS_N=r'\d{4}\.\d{5}\,N'
+GPS_E=r'\d{5}\.\d{5}\,E'
+
+HOST="192.168.1.100"
 PORT=21567
 BUFSIZ=2048
 ADDR=(HOST,PORT)
@@ -10,18 +15,41 @@ tcpSerSock=socket(AF_INET,SOCK_STREAM)
 tcpSerSock.bind(ADDR)
 tcpSerSock.listen(5)
 
-while True:
-    print("Waiting for connection...")
-    tcpCliSock,addr=tcpSerSock.accept()
-    print("connectes from :",addr)
+def re_gps(re_demo,gps_source):
+    gps_deal=re.search(re_demo,gps_source)
+    return gps_deal
 
+    
+def display(time,east,north):
+    if not (time or east or north):
+        print('GPS signal-loss...')
+    else:
+        real_time=int(time.group(1)[0:6])+80000
+        real_time=str(real_time)
+        print(real_time[0:2]+':'+real_time[2:4],east.group(0),north.group(0))
+
+        
+def main():
     while True:
-        data=tcpCliSock.recv(BUFSIZ)
-        if not data:
-            break
-        i=bytes(data).decode('ascii')
-        #i=str(data)
-        print(i)
-        print(data)
+        print("Waiting for connection...")
+        tcpCliSock,addr=tcpSerSock.accept()
+        print("connectes from :",addr)
+
+        while True:
+            data=tcpCliSock.recv(BUFSIZ)
+            if not data:
+                break
+            i=bytes(data).decode('ascii')
+            North=re_gps(GPS_N,i)
+            East=re_gps(GPS_E,i)
+            Time=re_gps(GPS_time,i)
+            display(Time,East,North)
+            #print(i)
+        tcpCliSock.close()
     tcpCliSock.close()
-tcpCliSock.close()
+if __name__=="__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('The program runs to completion!')
+        
