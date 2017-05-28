0a1c7d4973a338db979a4ff6fe0a02fac889755a
NEO <su5858@vip.qq.com>
2017-05-28 12:02:58 +0800

    爬虫详解

--- Spider/University.py
+++ Spider/University.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import bs4
 
+#爬取相应url
 def getHTMLText(url):
     try:
         r=requests.get(url,timeout=30)
@@ -11,14 +12,16 @@ def getHTMLText(url):
     except:
         return ""
 
+#解析HTML文件并填充
 def fillUnivList(ulist,html):
     soup=BeautifulSoup(html,"html.parser")
-    for tr in soup.find('tbody').children:
-        if isinstance(tr,bs4.element.Tag):
-            tds=tr('td')
+    for tr in soup.find('tbody').children:          #soup.find方法，只找到第一个符合条件的标签
+        if isinstance(tr,bs4.element.Tag):          #isinstance判断tr标签是否为Tag类型
+            tds=tr('td')                            #此处为简写形式，实为find_all方法
             ulist.append([tds[0].string,tds[1].string,tds[2].string])            
     pass
 
+#打印大学列表函数
 def printUnivList(ulist,num):
     tplt="{0:^10}\t{1:{3}^10}\t{2:^10}"
     print(tplt.format("排名","大学名称","成绩",chr(12288)))
@@ -33,6 +36,6 @@ def main():
     url='http://www.zuihaodaxue.com/zuihaodaxuepaiming2016.html'
     html=getHTMLText(url)
     fillUnivList(uinfo,html)
-    printUnivList(uinfo,20)     #20 univs
+    printUnivList(uinfo,20)     #只显示20所大学信息
 
 main()
