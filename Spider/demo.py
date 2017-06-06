790fdb6013f0ec659681f1e0b5da70c845f64504
NEO <su5858@vip.qq.com>
2017-06-06 17:09:24 +0800

    爬虫

--- Spider/demo.py
+++ Spider/demo.py
@@ -4,14 +4,14 @@ from bs4 import BeautifulSoup
 r=requests.get("http://python123.io/ws/demo.html")
 demo=r.text
 soup=BeautifulSoup(demo,"html.parser")
-
+'''
 for parent in soup.a.parents:
     if parent is None:
         print(parent)
     else:
         print(parent.name)
+'''
 
-
-print(soup.prettify())
+#print(soup.prettify())
 
 
